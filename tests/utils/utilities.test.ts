ea6faa154af069f0669329920d78f21217f088db
sheygs <macbookpro@sheygs-macbook.local>
2024-05-28 18:47:48 +0100

    chore(): paginate util tests

--- tests/utils/utilities.test.ts
+++ tests/utils/utilities.test.ts
@@ -4,7 +4,7 @@ import {
   ethToUSD,
   WEI_IN_ETHER,
   weiToETH,
-  // paginate,
+  paginate,
 } from '../../src/utils';
 
 describe('Ethereum conversion functions', () => {
@@ -63,111 +63,111 @@ describe('Ethereum conversion functions', () => {
   });
 });
 
-// describe('paginate function', () => {
-//   const mockResults = [
-//     {
-//       from: 'sender1',
-//       to: 'receiver1',
-//       blockNumber: '1',
-//       blockHash: 'hash1',
-//       hash: 'hash1',
-//       gasPrice: '100',
-//       value: '10',
-//     },
-//     {
-//       from: 'sender2',
-//       to: 'receiver2',
-//       blockNumber: '2',
-//       blockHash: 'hash2',
-//       hash: 'hash2',
-//       gasPrice: '200',
-//       value: '20',
-//     },
-//     {
-//       from: 'sender3',
-//       to: 'receiver3',
-//       blockNumber: '3',
-//       blockHash: 'hash3',
-//       hash: 'hash3',
-//       gasPrice: '300',
-//       value: '30',
-//     },
-//     {
-//       from: 'sender4',
-//       to: 'receiver4',
-//       blockNumber: '4',
-//       blockHash: 'hash4',
-//       hash: 'hash4',
-//       gasPrice: '400',
-//       value: '40',
-//     },
-//     {
-//       from: 'sender5',
-//       to: 'receiver5',
-//       blockNumber: '5',
-//       blockHash: 'hash5',
-//       hash: 'hash5',
-//       gasPrice: '500',
-//       value: '50',
-//     },
-//   ];
+describe('pagination function', () => {
+  const mockResults = [
+    {
+      from: 'sender1',
+      to: 'receiver1',
+      blockNumber: '1',
+      blockHash: 'hash1',
+      hash: 'hash1',
+      gasPrice: '100',
+      value: '10',
+    },
+    {
+      from: 'sender2',
+      to: 'receiver2',
+      blockNumber: '2',
+      blockHash: 'hash2',
+      hash: 'hash2',
+      gasPrice: '200',
+      value: '20',
+    },
+    {
+      from: 'sender3',
+      to: 'receiver3',
+      blockNumber: '3',
+      blockHash: 'hash3',
+      hash: 'hash3',
+      gasPrice: '300',
+      value: '30',
+    },
+    {
+      from: 'sender4',
+      to: 'receiver4',
+      blockNumber: '4',
+      blockHash: 'hash4',
+      hash: 'hash4',
+      gasPrice: '400',
+      value: '40',
+    },
+    {
+      from: 'sender5',
+      to: 'receiver5',
+      blockNumber: '5',
+      blockHash: 'hash5',
+      hash: 'hash5',
+      gasPrice: '500',
+      value: '50',
+    },
+  ];
 
-//   it('should paginate correctly with default values', () => {
-//     const result = paginate(mockResults);
-//     expect(result.totalCounts).toBe(5);
-//     expect(result.itemsPerPage).toBe(10); // default limit
-//     expect(result.hasPreviousPage).toBe(false);
-//     expect(result.hasNextPage).toBe(false);
-//     expect(result.currentPage).toBe(1);
-//     expect(result.results).toEqual(mockResults);
-//   });
+  it('should paginate correctly with default values', () => {
+    const result = paginate(mockResults);
+    expect(result.totalCounts).toBe(5);
+    expect(result.itemsPerPage).toBe(10); // default limit
+    expect(result.hasPreviousPage).toBe(false);
+    expect(result.hasNextPage).toBe(false);
+    expect(result.currentPage).toBe(1);
+    expect(result.results).toEqual(mockResults);
+  });
 
-//   it('should paginate correctly with custom page and limit', () => {
-//     const result = paginate(mockResults, 2, 2);
-//     expect(result.totalCounts).toBe(5);
-//     expect(result.itemsPerPage).toBe(2);
-//     expect(result.hasPreviousPage).toBe(true);
-//     expect(result.hasNextPage).toBe(true);
-//     expect(result.currentPage).toBe(2);
-//     expect(result.results).toEqual([
-//       {
-//         from: 'sender3',
-//         to: 'receiver3',
-//         blockNumber: '3',
-//         blockHash: 'hash3',
-//         hash: 'hash3',
-//         gasPrice: '300',
-//         value: '30',
-//       },
-//       {
-//         from: 'sender4',
-//         to: 'receiver4',
-//         blockNumber: '4',
-//         blockHash: 'hash4',
-//         hash: 'hash4',
-//         gasPrice: '400',
-//         value: '40',
-//       },
-//     ]);
-//   });
+  it('should paginate correctly with custom page and limit', () => {
+    const result = paginate(mockResults, 2, 2);
+    expect(result.totalCounts).toBe(5);
+    expect(result.itemsPerPage).toBe(2);
+    expect(result.hasPreviousPage).toBe(true);
+    expect(result.hasNextPage).toBe(true);
+    expect(result.currentPage).toBe(2);
+    expect(result.results).toEqual([
+      {
+        from: 'sender3',
+        to: 'receiver3',
+        blockNumber: '3',
+        blockHash: 'hash3',
+        hash: 'hash3',
+        gasPrice: '300',
+        value: '30',
+      },
+      {
+        from: 'sender4',
+        to: 'receiver4',
+        blockNumber: '4',
+        blockHash: 'hash4',
+        hash: 'hash4',
+        gasPrice: '400',
+        value: '40',
+      },
+    ]);
+  });
 
-//   it('should handle edge cases', () => {
-//     const result = paginate(mockResults, 3, 2);
-//     expect(result.totalCounts).toBe(5);
-//     expect(result.itemsPerPage).toBe(2);
-//     expect(result.hasPreviousPage).toBe(true);
-//     expect(result.hasNextPage).toBe(false); // Only 5 items, so no next page
-//     expect(result.currentPage).toBe(3);
-//     expect(result.results).toEqual([
-//       {
-//         from: 'sender5',
-//         to: 'receiver5',
-//         blockNumber: '5',
-//         blockHash: 'hash5',
-//         hash: 'hash5',
-//         gasPrice: '500',
-//         value: '50',
-//       },
-//     ]);
-//   });
-// });
+  it('should handle edge cases', () => {
+    const result = paginate(mockResults, 3, 2);
+    expect(result.totalCounts).toBe(5);
+    expect(result.itemsPerPage).toBe(2);
+    expect(result.hasPreviousPage).toBe(true);
+    expect(result.hasNextPage).toBe(false); // Only 5 items, so no next page
+    expect(result.currentPage).toBe(3);
+    expect(result.results).toEqual([
+      {
+        from: 'sender5',
+        to: 'receiver5',
+        blockNumber: '5',
+        blockHash: 'hash5',
+        hash: 'hash5',
+        gasPrice: '500',
+        value: '50',
+      },
+    ]);
+  });
+});
