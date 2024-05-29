c8aa70822716937bc4a98653996c47eebee669fe
sheygs <macbookpro@sheygs-macbook.local>
2024-05-29 14:20:11 +0100

    clean comments

--- src/database/repositories/index.ts
+++ src/database/repositories/index.ts
@@ -10,9 +10,7 @@ import {
 export interface ID {
   id: string;
 }
-// implement caching on DB lvel
-// pass key as parameter
-// and DB calls
+
 export class UniversalRepository<T extends ObjectLiteral> {
   constructor(private readonly entity: Repository<T>) {}
 
